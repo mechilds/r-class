a46c733d0b45bd5c0640a6408e3c92f6a4ff92f2
mechilds <mechilds14@gmail.com>
2017-08-07 13:15:08 -0400

    Add files via upload

---
+++ R Homework Week 1 - 2.R
@@ -0,0 +1,23 @@
+#Question 1
+myfact <- 1
+for(i in 1:12)
+{
+ myfact <- myfact*i
+  print(myfact)
+}
+
+
+#Question2
+
+vec1 <- c(20:50)
+vec2 <- vec1[c(1, 6, 11, 16, 21, 26, 31)]
+vec2
+
+# Question 3
+
+a <- 2
+b <- 7
+c <- 4
+x <- (-b + c(-1, 1) * sqrt(b^2 - 4*a*c))/(2*a)
+x
+
